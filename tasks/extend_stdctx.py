75abd817f8b98d6e8c179aa7f2b18416bd86c4bd
Brian Galdino <bgaldino@mac.com>
2024-03-10 20:07:03 -0700

    pythonic optimized

--- tasks/extend_stdctx.py
+++ tasks/extend_stdctx.py
@@ -6,36 +6,25 @@ from abc import abstractmethod
 
 class ExtendStandardContext(SFDXBaseTask):
     keychain_class = BaseProjectKeychain
+    
     task_options = {
         'access_token': {
-            'description': 'The access token for the org.  Defaults to the project default',
+            'description': 'The access token for the org. Defaults to the project default',
         }
     }
-    def _init_options(self, kwargs):
-        super(ExtendStandardContext, self)._init_options(kwargs)
-        self.env = self._get_env()
-    
-    def _prepruntime(self, a):
-        self._load_keychain()
-        # if not passed in - fall back to the key ring data
-        if "accesstoken" not in self.options or not self.options["accesstoken"]:
-            self.accesstoken = self.org_config.access_token
-        else:
-            self.accesstoken = self.options["accesstoken"]
 
-        if "instanceurl" not in self.options or not self.options["instanceurl"]:
-            self.instanceurl = self.org_config.instance_url
-        else:
-            self.instanceurl = self.options["instanceurl"]
+    def _init_options(self, kwargs):
+        super()._init_options(kwargs)
+        self.env = self._get_env()
+
     @property
     def keychain_cls(self):
-        klass = self.get_keychain_class()
-        return klass or self.keychain_class
+        return self.get_keychain_class() or self.keychain_class
 
     @abstractmethod
     def get_keychain_class(self):
         return None
-    
+
     @property
     def keychain_key(self):
         return self.get_keychain_key()
@@ -43,31 +32,32 @@ class ExtendStandardContext(SFDXBaseTask):
     @abstractmethod
     def get_keychain_key(self):
         return None
-    
+
     def _load_keychain(self):
-        if hasattr(self, 'keychain') == True and not self.keychain is None:
+        if hasattr(self, 'keychain') and self.keychain:
             return
 
         keychain_key = self.keychain_key if self.keychain_cls.encrypted else None
-
-        if self.project_config is None:
-            self.keychain = self.keychain_cls(self.universal_config, keychain_key)
-        else:
-            self.keychain = self.keychain_cls(self.project_config, keychain_key)
+        self.keychain = self.keychain_cls(self.project_config or self.universal_config, keychain_key)
+        
+        if self.project_config:
             self.project_config.keychain = self.keychain
-    def _run_task(self):
-        self._prepruntime(self)
-        self._extend_context_definition()
-    
-    def _extend_context_definition(self):
-        url = f"{self.org_config.instance_url}/services/data/v{self.project_config.project__package__api_version}/connect/context-definitions"
-        self.logger.info(f"Request URL: {url}")
-        headers = {
-            "Authorization": f"Bearer {self.org_config.access_token}",
-            "Content-Type": f"application/json",
-        }
 
-        # Prepare JSON payload
+    def _prepruntime(self):
+        self._load_keychain()
+        self.accesstoken = self.options.get("accesstoken") or self.org_config.access_token
+        self.instanceurl = self.options.get("instanceurl") or self.org_config.instance_url
+    
+    def _run_task(self):
+        self._prepruntime()
+        self._extend_context_definition()
+
+    def _extend_context_definition(self):
+        url = f"{self.instanceurl}/services/data/v{self.project_config.project__package__api_version}/connect/context-definitions"
+        headers = {
+            "Authorization": f"Bearer {self.accesstoken}",
+            "Content-Type": "application/json",
+        }
         payload = {
             "name": "RLM_SalesTransactionContext",
             "description": "Extension of Standard Sales Transaction Context",
@@ -75,45 +65,27 @@ class ExtendStandardContext(SFDXBaseTask):
             "baseReference": "SalesTransactionContext__stdctx",
             "startDate": "2024-01-01T00:00:00.000Z"
         }
-        json_payload = json.dumps(payload)
-        self.logger.info(f"Request body: {json_payload}")
+        response = requests.post(url, headers=headers, json=payload)
 
-        # Construct JSON request body
-        body = (
-            f"{json_payload}\r\n"
-        ).encode("utf-8")
+        if response.ok:
+            response_json = response.json()
+            context_id = response_json.get('contextDefinitionId')
 
-        response = requests.post(url, headers=headers, data=body)
-        response_json = response.json()
-
-        if response.status_code == 201:
-            self.logger.info("Deployment request successful")
-            #self.logger.info(f"Response: {response_json}")
-            self.logger.info(f"Context ID: {response_json['contextDefinitionId']}")
-            context_id = response_json['contextDefinitionId']
-            if context_id is not None:
+            if context_id:
+                self.logger.info(f"Context ID: {context_id}")
                 self.context_id = context_id
                 response_json = self._get_context_mapping()
-                 # Attempt to access nested data with error handling.
                 version_list = response_json.get('contextDefinitionVersionList', [])
 
-                # Checking if 'contextDefinitionVersionList' is not empty.
                 if version_list:
                     context_mappings = version_list[0].get('contextMappings', [])
-                
-                   # Initialize to None.
-                    sales_transaction_mapping_id = None
-                    # Iterate over 'contextMappings' to find the desired 'contextMappingId'.
-                    for mapping in context_mappings:
-                        if mapping.get("name") == "SalesTransaction":
-                            self.sales_transaction_mapping_id = mapping.get('contextMappingId')
-                            break
-                    if self.sales_transaction_mapping_id:
-                        # Log the successful retrieval of information.
-                        self.logger.info(
-                            f"Sales Transaction Context Mapping ID: {self.sales_transaction_mapping_id}"
-                        )
-                        # Update the context mapping
+                    sales_transaction_mapping_id = next(
+                        (m['contextMappingId'] for m in context_mappings if m.get("name") == "SalesTransaction"),
+                        None
+                    )
+                    if sales_transaction_mapping_id:
+                        self.sales_transaction_mapping_id = sales_transaction_mapping_id
+                        self.logger.info(f"Sales Transaction Context Mapping ID: {sales_transaction_mapping_id}")
                         self._update_context_mappings()
                         self._activate_context_id()
                     else:
@@ -123,88 +95,45 @@ class ExtendStandardContext(SFDXBaseTask):
             else:
                 self.logger.error("No context ID found")
         else:
-            self.logger.error(
-                f"Create request failed with status code {response.status_code}"
-            )
+            self.logger.error(f"Create request failed with status code {response.status_code}")
 
     def _update_context_mappings(self):
-        url = f"{self.org_config.instance_url}/services/data/v{self.project_config.project__package__api_version}/connect/context-definitions/{self.context_id}/context-mappings"
-        self.logger.info(f"Request URL: {url}")
+        url = f"{self.instanceurl}/services/data/v{self.project_config.project__package__api_version}/connect/context-definitions/{self.context_id}/context-mappings"
         headers = {
-            "Authorization": f"Bearer {self.org_config.access_token}",
-            "Content-Type": f"application/json",
+            "Authorization": f"Bearer {self.accesstoken}",
+            "Content-Type": "application/json",
         }
-
-        # Prepare JSON payload
         payload = {
             "contextMappings": [
                 {
-                    "contextMappingId": f"{self.sales_transaction_mapping_id}",
+                    "contextMappingId": self.sales_transaction_mapping_id,
                     "isDefault": "true",
                     "name": "SalesTransaction"
                 }
             ]
         }
-        json_payload = json.dumps(payload)
-        self.logger.info(f"Request body: {json_payload}")
+        response = requests.patch(url, headers=headers, json=payload)
 
-        # Construct JSON request body
-        body = (
-            f"{json_payload}\r\n"
-        ).encode("utf-8")
+        if not response.ok:
+            self.logger.error(f"Patch request failed with status code {response.status_code}")
 
-        response = requests.patch(url, headers=headers, data=body)
-        response_json = response.json()
-
-        if response.status_code == 200:
-            self.logger.info("Patch request successful")
-            #self.logger.info(f"Response: {response_json}")
-        else:
-            self.logger.error(
-                f"Patch request failed with status code {response.status_code}"
-            )
-    
     def _activate_context_id(self):
-        url = f"{self.org_config.instance_url}/services/data/v{self.project_config.project__package__api_version}/connect/context-definitions/{self.context_id}"
-        self.logger.info(f"Request URL: {url}")
+        url = f"{self.instanceurl}/services/data/v{self.project_config.project__package__api_version}/connect/context-definitions/{self.context_id}"
         headers = {
-            "Authorization": f"Bearer {self.org_config.access_token}",
-            "Content-Type": f"application/json",
+            "Authorization": f"Bearer {self.accesstoken}",
+            "Content-Type": "application/json",
         }
+        payload = {"isActive": "true"}
+        response = requests.patch(url, headers=headers, json=payload)
 
-        # Prepare JSON payload
-        payload = {
-            "isActive": "true"
-        }
-        json_payload = json.dumps(payload)
-        self.logger.info(f"Request body: {json_payload}")
-
-        # Construct JSON request body
-        body = (
-            f"{json_payload}\r\n"
-        ).encode("utf-8")
-
-        response = requests.patch(url, headers=headers, data=body)
-        response_json = response.json()
-
-        if response.status_code == 200:
-            self.logger.info("Patch request successful")
-            #self.logger.info(f"Response: {response_json}")
-        else:
-            self.logger.error(
-                f"Patch request failed with status code {response.status_code}"
-            )
+        if not response.ok:
+            self.logger.error(f"Patch request failed with status code {response.status_code}")
 
     def _get_context_mapping(self):
-        url = f"{self.org_config.instance_url}/services/data/v{self.project_config.project__package__api_version}/connect/context-definitions/{self.context_id}"
-        self.logger.info(f"Request URL: {url}")
+        url = f"{self.instanceurl}/services/data/v{self.project_config.project__package__api_version}/connect/context-definitions/{self.context_id}"
         headers = {
-            "Authorization": f"Bearer {self.org_config.access_token}",
-            "Content-Type": f"application/json",
+            "Authorization": f"Bearer {self.accesstoken}",
+            "Content-Type": "application/json",
         }
         response = requests.get(url, headers=headers)
-        response_json = response.json()
-        #self.logger.info(f"Response: {response_json}")
-        return response_json
-
-    
+        return response.json() if response.ok else {}
